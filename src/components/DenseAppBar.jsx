951e39f18e45cd12159e23d6ce2e48e52538f880
DivyanshSareen <divyansh@LAPTOP-MNECQT85.localdomain>
2023-07-06 20:53:31 +0530

    Added sidebar navigation

--- src/components/DenseAppBar.jsx
+++ src/components/DenseAppBar.jsx
@@ -6,6 +6,14 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useLocation } from 'react-router';
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+} from '@mui/material';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function getAppBarTitle(path) {
   let titleMap = {
@@ -18,16 +26,70 @@ function getAppBarTitle(path) {
 
 export default function DenseAppBar() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Drawer
+        anchor='left'
+        open={isOpen}
+        onClick={() =>
+          setIsOpen((isOpen) => !isOpen)
+        }>
+        <List
+          sx={{
+            width: '250px',
+          }}>
+          <ListItem sx={{ fontWeight: 'bold' }}>
+            Task 1
+          </ListItem>
+          <Link to='/todo-list'>
+            <ListItem>Todo</ListItem>
+          </Link>
+          <Divider />
+          <ListItem sx={{ fontWeight: 'bold' }}>
+            Task 2
+          </ListItem>
+          <Link to='/listing'>
+            <ListItem>Product Listing</ListItem>
+          </Link>
+          <Link to='/cart'>
+            <ListItem>Product Cart</ListItem>
+          </Link>
+          <Divider />
+          <ListItem sx={{ fontWeight: 'bold' }}>
+            Task 3
+          </ListItem>
+          <Link to='/pokemon'>
+            <ListItem>
+              PokeAPI Integration
+            </ListItem>
+          </Link>
+          <Divider />
+          <ListItem sx={{ fontWeight: 'bold' }}>
+            Task 4
+          </ListItem>{' '}
+          <Link to='/'>
+            <ListItem>Home</ListItem>
+          </Link>
+          <Link to='/about'>
+            <ListItem>About</ListItem>
+          </Link>
+          <Link to='/contact'>
+            <ListItem>Contact</ListItem>
+          </Link>
+        </List>
+      </Drawer>
       <AppBar position='static'>
         <Toolbar variant='dense'>
           <IconButton
             edge='start'
             color='inherit'
             aria-label='menu'
-            sx={{ mr: 2 }}>
+            sx={{ mr: 2 }}
+            onClick={() => {
+              setIsOpen((isOpen) => !isOpen);
+            }}>
             <MenuIcon />
           </IconButton>
           <Typography
